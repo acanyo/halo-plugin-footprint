b5aa460c6cd0355d782e5c78f9a29c007a20bac5
acanyo <30819792@qq.com>
2025-03-23 12:18:46 +0800

    fix:删除后端无用代码

--- src/main/java/cc/lik/footprint/FootprintHeadProcessor.java
+++ src/main/java/cc/lik/footprint/FootprintHeadProcessor.java
@@ -10,7 +10,6 @@ import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.processor.element.IElementModelStructureHandler;
 import reactor.core.publisher.Mono;
 import run.halo.app.theme.dialect.TemplateHeadProcessor;
-import cc.lik.footprint.service.FootprintService;
 
 import java.util.Properties;
 
@@ -22,34 +21,24 @@ public class FootprintHeadProcessor implements TemplateHeadProcessor {
         new PropertyPlaceholderHelper("${", "}");
 
     private final PluginWrapper pluginWrapper;
-    private final FootprintService footprintService;
 
     @Override
     public Mono<Void> process(ITemplateContext context, IModel model,
         IElementModelStructureHandler structureHandler) {
         final IModelFactory modelFactory = context.getModelFactory();
         
-        return footprintService.getConfigByGroupName()
-            .map(settings -> {
-                Properties properties = new Properties();
-                properties.setProperty("version", pluginWrapper.getDescriptor().getVersion());
-                properties.setProperty("amapKey", settings.getGaoDeKey());
+        Properties properties = new Properties();
+        properties.setProperty("version", pluginWrapper.getDescriptor().getVersion());
 
-                String script = PROPERTY_PLACEHOLDER_HELPER.replacePlaceholders("""
-                    <!-- footprint start -->
-                    <link rel="stylesheet" type="text/css" href="/plugins/footprint/assets/static/css/footprint.css?version=${version}" />
-                    <link rel="stylesheet" type="text/css" href="/plugins/footprint/assets/static/font/result.css?version=${version}" />
-                    <script type="text/javascript" src="/plugins/footprint/assets/static/js/footprint.js?version=${version}"></script>
-                    <!-- footprint end -->
-                    """, properties);
-                
-                model.add(modelFactory.createText(script));
-                return null;
-            })
-            .then()
-            .onErrorResume(e -> {
-                // 发生错误时返回空
-                return Mono.empty();
-            });
+        String script = PROPERTY_PLACEHOLDER_HELPER.replacePlaceholders("""
+            <!-- footprint start -->
+            <link rel="stylesheet" type="text/css" href="/plugins/footprint/assets/static/css/footprint.css?version=${version}" />
+            <link rel="stylesheet" type="text/css" href="/plugins/footprint/assets/static/font/result.css?version=${version}" />
+            <script type="text/javascript" src="/plugins/footprint/assets/static/js/footprint.js?version=${version}"></script>
+            <!-- footprint end -->
+            """, properties);
+        
+        model.add(modelFactory.createText(script));
+        return Mono.empty();
     }
 }
