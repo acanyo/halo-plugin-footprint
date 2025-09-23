8cb83bad0f90b20edf36ade64a747f4089220669
Handsome <30819792@qq.com>
2025-09-23 15:27:34 +0800

    后端控制台新增访问前台按钮

--- ui/src/views/Footprint.vue
+++ ui/src/views/Footprint.vue
@@ -232,6 +232,13 @@ const handleFetchData = () => {
 const handleEdit = (row: Footprint) => {
   handleOpenCreateModal(row);
 };
+
+const visitFrontend = () => {
+  // 获取当前域名，构建前台页面URL
+  const currentOrigin = window.location.origin;
+  const frontendUrl = `${currentOrigin}/footprints`;
+  window.open(frontendUrl, '_blank');
+};
 </script>
 
 <template>
@@ -244,6 +251,17 @@ const handleEdit = (row: Footprint) => {
   <VPageHeader title="足迹">
     <template #actions>
       <VSpace>
+        <VButton
+          type="primary"
+          @click="visitFrontend"
+        >
+          <template #icon>
+            <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+            </svg>
+          </template>
+          访问前台
+        </VButton>
         <VButton
           type="secondary"
           @click="editingModal = true"
